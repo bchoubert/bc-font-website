2b363750246bf362a3ba402c98211f23ecf69389
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2020-10-16 19:15:55 +0200

    feat(react+navbar): Initialized React and build the navbar

---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Icons.tsx';
+
+const Icons = () => {
+
+  return <div className="Icons">
+
+  </div>;
+};
+
+export default Icons;
---
+++ src/components/Install/Install.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Install.scss';
+
+const Install = () => {
+
+  return <div className="Install">
+
+  </div>;
+};
+
+export default Install;

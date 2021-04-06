38657027383c3f15b34a3346ceacce7cc33c51bc
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2021-04-06 11:22:13 +0200

    Updates

--- bundle.js
+++ bundle.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://bchoubert.github.io/bc-font-website/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
